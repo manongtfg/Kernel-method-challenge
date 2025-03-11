f3e890ceb77c9a5c46540218fe30391d32c5a559
Manon Gouttefangeas <manon.gtfg@gmail.com>
2025-03-11 16:15:36 +0100

    Modification du fichier start.py

--- start.py
+++ start.py
@@ -9,22 +9,33 @@ def main():
     # Training step 
     X_train, labels, labels_onehot = preprocess("data/Xtr0.csv", "data/Xtr1.csv", "data/Xtr2.csv", labels_1="data/Ytr0.csv", labels_2="data/Ytr1.csv", labels_3="data/Ytr2.csv")
 
-    # Training of the staking model
-    base_models = [kernel_ridge_regression(lambda_=0.3, kernel='spectrum', num_classes=2, k=7),
-               kernel_logistic_regression(alpha0_coeff = 0, lambda_ = 5, k=3, kernel='spectrum', n_iter=10)]
-    meta_model = kernel_ridge_regression(lambda_=0.15, kernel='linear', num_classes=1)
+    # Training of the STAKING MODEL 
+    # base_models = [kernel_ridge_regression(lambda_=0.3, kernel='spectrum', num_classes=2, k=7),
+    #            kernel_logistic_regression(alpha0_coeff = 0, lambda_ = 5, k=3, kernel='spectrum', n_iter=10)]
+    # meta_model = kernel_ridge_regression(lambda_=0.15, kernel='linear', num_classes=1)
     
-    model = StackingModel(base_models, meta_model)
-    model.fit(X_train, labels, labels_onehot)
+    # model = StackingModel(base_models, meta_model)
+    # model.fit(X_train, labels, labels_onehot)
+
+    # Training of the KERNEL RIDGE REGRESSION MODEL WITH SPECTRUM KERNEL
+    model = kernel_ridge_regression(lambda_=0.3, kernel='spectrum', num_classes=2, k=7)
+    model.fit(X_train, labels_onehot)
 
     # Testing step
-    X_test = preprocess("Xte0.csv", "Xte1.csv", "Xte2.csv", train=False)
+    X_test = preprocess("data/Xte0.csv", "data/Xte1.csv", "data/Xte2.csv", train=False)
 
     # Make prediction
     Y_pred = model.predict(X_test)
+    N = len(Y_pred)
 
-    # Sauvegarder les résultats
-    pd.DataFrame(Y_pred).to_csv("data/Yte.csv", index=False, header=False)
+    df = pd.DataFrame({
+    "Id": np.arange(0, N, 1),
+    "Bound": Y_pred 
+    })
+
+    # Save in a CSV file
+    df.to_csv("data/Yte.csv", index=False)
+
     print("Predictions saved in Yte.csv")
 
 
